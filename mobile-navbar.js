212284ab721e2927caba88dedc557d56db98910a
Anderson Silva <andsjj07@gmail.com>
2025-01-08 16:05:19 -0300

    Update mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -39,3 +39,14 @@ const mobileNavbar = new MobileNavbar(
     '.nav-list li',
 );
 mobileNavbar.Init();
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area-1', {rotate: {x: 0, y: 90, z: 0}},
+     { duration: 2000} );
+sr.reveal('.area-2',{rotate: {x: 90, y: 90, z: 0}},
+     { duration: 2000});
+sr.reveal('.area-3', {rotate: {x: 0, y: 100, z: 90}}, 
+{ duration: 2000});
+sr.reveal('.area-4', {rotate: {x: 0, y: 100, z: 90}}, 
+{ duration: 2000});
